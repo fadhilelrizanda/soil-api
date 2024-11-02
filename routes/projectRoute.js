7f0e03baf8738ddab85c26e236b20a854e1490d2
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-11-02 15:17:09 +0700

    add debugging

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -59,17 +59,19 @@ router.put("/update/project/:id", async (req, res) => {
 });
 
 // get data from specific project Data
+// Get a specific project by ID
 router.get("/project/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const projects = await Project.findById(id);
-    res.json(projects);
+    const project = await Project.findById(id);
 
-    if (!projects) {
+    if (!project) {
       return res.status(404).json({ message: "Project not found" });
     }
+
+    res.json(project);
   } catch (error) {
-    console.error("Error fetching projects:", error);
+    console.error("Error fetching project:", error);
     res.status(500).json({ message: error.message });
   }
 });
